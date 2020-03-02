0a77dbf9bcbe6df000b7617acab4136c7bcd6d8e
ctoppel <ctoppel@aol.com>
2020-03-01 20:47:55 -0800

    updated mock data, added portfolio styles

--- src/routes/Hairdresser/Hairdresser.js
+++ src/routes/Hairdresser/Hairdresser.js
@@ -8,43 +8,65 @@ import hairdressers from "../../mockData.json"
 
 
 const Hairdressers = () => {
-  let { id } = useParams();
+  const { id: hairdresserId } = useParams();
+  console.log(hairdresserId)
+  const foundHairdresser = hairdressers.find(({ id }) => {console.log('asfd', id); return id === Number(hairdresserId)})
+
+  // Show error if no hairdresser found in list
+  if (!foundHairdresser) return <div>No Hairdressser Found</div>
+
+  const {
+    id,
+    firstName,
+    lastName,
+    gender,
+    location,
+    bio,
+    imgUrl,
+    price,
+    visitCount,
+    rating,
+    portfolioImages,
+  } = foundHairdresser
 
   return (
-    <div style={{width: '100%'}}>
-      <Coverflow
-        displayQuantityOfSide={2}
-        navigation={false}
-        infiniteScroll={false}
-        enableHeading
-        media={{
-          '@media (max-width: 768px)': {
-            height: '300px'
-          },
-          '@media (min-width: 768px)': {
-            height: '600px'
-          },
-          '@media (min-width: 1200px)': {
-            height: '800px'
-          }
-        }}
-      >
-        {hairdressers.map(({
-          id,
-          firstName,
-          lastName,
-          gender,
-          location,
-          bio,
-          imgUrl,
-          price,
-          visitCount,
-          rating,
-        }) => (
-          <img key={id} src={imgUrl} alt='Album one' />
-        ))}
-      </Coverflow>
-      {id}
+    <div style={{ display: 'flex', flexDirection: 'column', width: '100%' }}>
+      <div className="portfolio">
+        <div style={{ flex: 1, backgroundColor: 'rgba(0,0,0,0.1)'}} />
+        <div style={{width: '100%', maxWidth: 1920}}>
+          <Coverflow
+            displayQuantityOfSide={2}
+            navigation={false}
+            infiniteScroll={false}
+            enableHeading
+            media={{
+              '@media (max-width: 640px)': {
+                height: '220px'
+              },
+              '@media (min-width: 640px)': {
+                height: '300px'
+              },
+              '@media (min-width: 768px)': {
+                height: '400px'
+              },
+              '@media (min-width: 960px)': {
+                height: '500px'
+              },
+              '@media (min-width: 1200px)': {
+                height: '650px'
+              }
+            }}
+          >
+            {portfolioImages.map(({ url, description }) => (
+              <img key={id} src={url} alt={description} />
+            ))}
+          </Coverflow>
+        </div>
+        <div style={{ flex: 1, backgroundColor: 'rgba(0,0,0,0.1)'}} />
+      </div>
+      <div style={{ backgroundColor: 'black', flex: 1 }}>
+          <div>{firstName}{lastName}</div>
+      </div>
     </div>
   )
 }
